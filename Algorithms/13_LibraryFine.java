b27fcc3f5ec4135026b349014d3b4e26ae679d39
Andy Wong <wongandy97@gmail.com>
2017-03-22 23:52:05 -0400

    Day eight algorithm

---
+++ Algorithms/13_LibraryFine.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class LibraryFine {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        //Date Returned
+        int d1 = in.nextInt();
+        int m1 = in.nextInt();
+        int y1 = in.nextInt();
+        //Date expected of return
+        int d2 = in.nextInt();
+        int m2 = in.nextInt();
+        int y2 = in.nextInt();
+        
+        int fine = 0;
+        
+        if(d1 == d2 && m1 == m2 && y1 == y2) //CASE 1: If returned day of
+        {
+            fine = 0;
+        }
+        else if ( d1 != d2 && m1 == m2 && y1 == y2)
+        {
+            fine = 15 * (d1 - d2);
+        }
+        else if(m1 != m2 && y1 == y2){
+            fine = 500 * (m1 - m2);
+        }
+        else if(y1 != y2){
+            fine = 10000;
+        }
+        
+        System.out.println(fine);
+    }
+}
