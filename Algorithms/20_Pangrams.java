e03c7f5bf053a80752ace057ad4365c8a483741a
Andy Wong <wongandy97@gmail.com>
2017-03-31 22:03:18 -0400

    Fix pangrams program

--- Algorithms/20_Pangrams.java
+++ Algorithms/20_Pangrams.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
 
 public class Pangrams
 {
     public static void main(String []args)
     {
-        
+        Scanner in = new Scanner(System.in);
+    }
+    
+    protected static String getOutput(String s) //No public access, availabe for subclass, package and everything but world. 
+    {
+        return (isPangram(s)) ? "pangram" : "not pangram";
+    }
+    
+    protected static boolean isPangram(String s)
+    {
+        String lowerCaseS = s.toLowerCase();
+        for(char letter = 'a'; letter <= 'z'; letter++)
+        {
+            if(lowerCaseS.indexOf(letter) < 0) return false; //if letter does not exist/not found, will return -1 which means, not in alphabet.
+        }
+        return true;
     }
 }
