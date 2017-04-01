71497f1a6221e2143c76bec60f860283a0037df4
Andy Wong <wongandy97@gmail.com>
2017-03-31 23:57:17 -0400

    Corrected Day 22 Algorithm

--- Algorithms/21_WeightedUniformString.java
+++ Algorithms/21_WeightedUniformString.java
@@ -1,58 +1,38 @@
 import java.util.Scanner;
+import java.util.HashSet;
 
 public class WeightedUniformString
 {
-    protected static boolean checkQueries(int queries)
-    {
-//        for(int b = 0; b < numOfQueries; b++)
-//        {
-//            //int count = b;//Don't think we need this.
-//            for(int c = 0; c < numOfQueries; c++)
-//            {
-//                if(weights[b] == weightResult[c])
-//                {
-//                    
-//                }
-//            }
-//        }
-        return false;
-    }
-    
-    protected static void weightedUniform(String a)
-    {
-        char[] b = a.toCharArray();
-        char[] c = new char[a.length()];
-        for(int i = 0; i < a.length();i++){//Check how many of the same letter is there. 
-            int e = i;
-            for(int d = 0; d < c.length; d++)
-            {
-                if(b[e] != c[d]);
-            }
-        }
-    }
-    
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
-        String input = in.nextLine();
+        String str = in.next();
         int numOfQueries = in.nextInt();
-        
-        weightedUniform(input);
-        
-//        int[] weights = new int[numOfQueries];//Query of expected weight 
-        int[] weightResult = new int[numOfQueries]; //Where we place the results from string uniform to perform check and produce result.
-        
-        for(int a = 0; a < numOfQueries; a++) //Add Weight result to array for easier check
+        HashSet<Integer> weights = getWeights(str);
+        while (0 < numOfQueries--) //Don't need to write for loop, just go down in numbers
         {
-//            int weight = in.nextInt();
-//            weights[a] = weight;
-            System.out.println(getOutput(a));
-        } 
-        
-        
+            int x = in.nextInt();
+            System.out.println(weights.contains(x) ? "Yes" : "No"); //Does weights Hashset contain result we want? 
+            //Use .contains() instead of writing for loop to interate through for loop and find that result, costs time. 
+        }
     }
-    protected static String getOutput(int queries)
+    
+    private static HashSet<Integer> getWeights(String str)
     {
-        return(checkQueries(queries)) ? "Yes" : "No";
+        HashSet<Integer> weights = new HashSet<>();
+        int weight = 0;
+        char previous = ' ';//Prevent matches 
+        for(int i = 0; i < str.length(); i++) //This will add the result everytime there is an additional same letter. 
+        {
+            char current = str.charAt(i);
+            if(current != previous) //If letter has no reoccurance, we will set weight to 0 and follow code before to add to HashSet
+            {
+                weight = 0;
+            }
+            weight += current - 'a' + 1; //ASCII table. z -a = 25, so need to add one for 26
+            weights.add(weight);
+            previous = current; //Sets previous to current so we can see if the next char is the smae to get weight result.
+        }
+        return weights;
     }
 }
