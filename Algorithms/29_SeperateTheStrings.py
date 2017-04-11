10eb030be0c7ca3ddc2f8a92a1ca5aadd32fefd7
Andy Wong <wongandy97@gmail.com>
2017-04-11 18:47:01 -0400

    UPDATE Algorithms

--- Algorithms/29_SeperateTheStrings.py
+++ Algorithms/29_SeperateTheStrings.py
@@ -7,13 +7,38 @@ import sys
 queries = int(raw_input().strip())
 words = []
 
-for i in range(queries):
+for i in range(queries): #Adds all elements into an array
     word = raw_input().strip()
     words.append(word)
     
-
-print words[0:queries]
+for word in words:
+    if(len(word) == 0 or len(word) == 1):
+        print ("NO")
+    else:
+        isBeauty = False
+        length = len(word)
+        sum = 0
+        for i in range(length):
+            sum = 0
+            check = str("")
+            if(i != length -1): #Make sure ware not testing at 0 when we do substring 
+                isBeauty = False
+                temp = word[:i]
+                if not(len(temp) * 2 > len(word)) and not(len(temp) == 0):
+                    code = int(float(temp))
+                    sum = code
+                    
+                    while len(check) < len(str):
+                        check = check + code
+                        code = code + 1
+                    if form.equals(str):
+                        isBeauty = True
+                        break  
+                else:
+                    continue
+            if(isValid):
+                print("YES" + sum)
+            else:
+                print("NO")
+            
     
-    
-    
-
