c646ec2ebfba5b39818d88d0a9b04ac512f85b37
Andy Wong <wongandy97@gmail.com>
2017-04-18 21:43:22 -0400

    Day 27 Algorithm Added

---
+++ Algorithms/31_2DArray.py
@@ -0,0 +1,16 @@
+arr = []
+max = 0
+for arr_i in range(6):
+   arr_t = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+   arr.append(arr_t)
+
+max = arr[0][0] + arr[0][1] + arr[0][2] + arr[1][1] + arr[2][0] + arr[2][1] + arr [2][2]
+
+for i in range(4):
+    for j in range(4):
+        temp = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1]+ arr[i+2][j+2]
+        if temp >= max:
+            max = temp
+            
+  
+print (max)
