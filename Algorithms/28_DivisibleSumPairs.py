f09fe29d23a90a8341954cd8cb9b8889a7d8a35a
Andy Wong <wongandy97@gmail.com>
2017-04-23 21:26:01 -0400

    Add algorithm to file

--- Algorithms/28_DivisibleSumPairs.py
+++ Algorithms/28_DivisibleSumPairs.py
@@ -2,3 +2,17 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 
+#!/bin/python3
+
+import sys
+
+n,k = input().strip().split(' ')
+n,k = [int(n),int(k)]
+a = [int(a_temp) for a_temp in input().strip().split(' ')]
+count = 0
+for b in range(n):
+    for i in range(n):
+        if a[b] < a[i] and (a[b] + a[i])%k == 0:
+            count+=1
+            
+print (count)
