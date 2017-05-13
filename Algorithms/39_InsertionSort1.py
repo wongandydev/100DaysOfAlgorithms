beb603a08b47a2693eccfa1f81803deb1f59dd6b
Andy Wong <wongandy97@gmail.com>
2017-05-12 20:39:08 -0400

    Add Algorithm 39

---
+++ Algorithms/39_InsertionSort1.py
@@ -0,0 +1,27 @@
+#!bin/python3
+
+size = int(input().strip())
+
+Arr = [int(arrItem) for arrItem in input().strip().split(' ')]
+
+e = Arr[len(Arr) - 1]
+
+tempIndex = 0
+
+#print(Arr, len(Arr), e)
+
+for i in range(size-1):
+    if e > Arr[i] and e < Arr[i+1]:
+        tempIndex = i+1
+        #print (tempIndex)
+
+hold = size - 1
+
+for hold in range(size - 1, tempIndex, -1):
+    Arr[hold] = Arr[hold - 1]
+    print(*Arr)
+Arr[tempIndex] = e
+
+print(*Arr)
+
+
