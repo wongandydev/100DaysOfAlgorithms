79e58b2396b382d8cd051902cbf475e48af72644
Andy Wong <wongandy97@gmail.com>
2017-05-25 11:19:56 -0400

    Update algorithm

--- Algorithms/46_QuickSort2.js
+++ Algorithms/46_QuickSort2.js
@@ -1,6 +1,35 @@
 function processData(input) {
     //Enter your code here
-} 
+    c = input.split('\n');
+    size = c[0];
+    arr = c[1].split(' ');
+    quickSort(arr);
+}
+
+function quickSort(ar) { //Recursion
+    if(ar.length < 2) { //If there is 1 element left, stop recursion. 
+        return ar;     
+    }
+
+    var left = [];
+    var right = [];
+
+    for(var i = 1; i < ar.length; i++){ //Add to left and right lists. 
+
+        if(ar[i] >= ar[0]){
+           right.push(ar[i]) ;
+        }
+        else if(ar[i] < ar[0]){
+           left.push(ar[i]);
+        }
+    }
+
+    var sortLeft = quickSort(left); //recursively sort left list
+    var sortRight = quickSort(right); //recursively sort right list.
+    var combine = sortLeft.concat(ar[0]).concat(sortRight); //put everything together starting with left, then middlle then right. 
+    console.log(combine.join(' ')); //prints combie with ' ' to join together with a space between. 
+    return combine;
+}
 
 process.stdin.resume();
 process.stdin.setEncoding("ascii");
