e33fbde27b9e7798b34a998367ac8f1b1e782326
Andy Wong <wongandy97@gmail.com>
2017-05-25 10:02:58 -0400

    Update algorithm with different solution

--- Algorithms/51_Anagram.js
+++ Algorithms/51_Anagram.js
@@ -16,12 +16,13 @@ function processData(input) {
             s1 = arr[j].substring(0, (arr[j].length)/2);
             s2 = arr[j].substring((arr[j].length)/2, (arr[j].length));
             
-            for (i in s1){
-                if (s2.includes(s1[i]) == false){
-                    count++;
+            for(var i = 0; i < s1.length; i++) {
+                if(s2.includes(s1[i]) == true) {
+                    index = s2.indexOf(s1[i]);
+                    s2 = s2.substring(0, index) + s2.substring(index+1);
                 }
-            }
-            console.log(count);
+           }
+            console.log(s2.length);
         }
     }
 } 
