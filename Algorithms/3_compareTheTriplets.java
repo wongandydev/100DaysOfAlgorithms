7ef687d9e44cf72ef7647e66772319054a7a3884
Andy Wong <wongandy97@gmail.com>
2017-08-27 09:40:39 -0400

    Change algorithm solution so something more efficient (less if else statements)

--- Algorithms/3_compareTheTriplets.java
+++ Algorithms/3_compareTheTriplets.java
@@ -1,6 +1,23 @@
 import java.util.*;
 
-public class compareTheTriplets {
+public class CompareTheTriplets {
+    static int[] solve(int a0, int a1, int a2, int b0, int b1, int b2){
+        int a = 0;
+        int b = 0;
+        int [] alice = {a0,a1,a2};
+        int [] bob = {b0, b1,b2};
+        
+        for (int i = 0; i < 3; i++){
+            if(alice[i] > bob[i]){
+                a++;
+            }
+            else if (alice[i] < bob[i]){
+                b++;
+            }
+        }
+        int[] result = {a,b};
+        return result;
+    }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -10,36 +27,12 @@ public class compareTheTriplets {
         int b0 = in.nextInt();
         int b1 = in.nextInt();
         int b2 = in.nextInt();
-        int a = 0; //Alice
-        int b = 0; //Bob
+        int[] result = solve(a0, a1, a2, b0, b1, b2);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + (i != result.length - 1 ? " " : ""));
+        }
+        System.out.println("");
         
-        if (a0 > b0)
-        {
-            a += 1;
-        }
-        else if (a0 < b0)
-        {
-            b += 1;
-        }
-        
-        if (a1 > b1)
-        {
-            a += 1;
-        }
-        else if (a1 < b1)
-        {
-            b += 1;
-        }
-        
-        if (a2 > b2)
-        {
-            a += 1;
-        }
-        else if (a2 < b2)
-        {
-            b += 1;
-        }
-        
-        System.out.println(a + " " + b);
+
     }
 }
