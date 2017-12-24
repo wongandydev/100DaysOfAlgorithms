4d3f38b7c83aa1f209554f2620a991df9cb4e04f
Andy Wong <wongandy97@gmail.com>
2017-12-23 22:28:22 -0500

    Add new solution, that actually completes all test. Previous solution only passes test case 1

--- Algorithms/56_CountingSort1.py
+++ Algorithms/56_CountingSort1.py
@@ -1,8 +1,22 @@
 
-size = int(input())
+#Best Solution - 
+
+size = int(input()) #Need this line here because it reads the first line input. W/O this, we can't read the next line of ints. 
+results = [0]*100 
+
+for i in [int(n) for n in input().split()]:
+    results[i] += 1
+
+print(*results)
+
+
+# <---------- SOLUTION BELOW ONLY WORKS FOR THE FIRST TEST CASE.>
+
+
+size = int(input()) #Need this line here because it reads the first line input. W/O this, we can't read the next line of ints. 
 results = [0] * size  #Creates an array of size k with zeroes 
 
-for i in [int(n) for n in input().split(' ')]: #second part of for loops first iterates through the input and makes them all an int in a list. Then iterate with i.
+for i in [int(n) for n in input().split()]: #second part of for loops first iterates through the input and makes them all an int in a list. Then iterate with i.
     results[i] += 1
 
 print(*results) #By placing the star in the front it basically prints the array as a string. Aka 'unpacks the list'
